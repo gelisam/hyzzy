d237e6fbfec96bc6fa4fff33b44b4fc5ba39fe2d
Samuel Gélineau <gelisam+github@gmail.com>
2020-06-20 15:12:43 -0400

    test suite
    I can't write it as a Haskell program because I can't call 'stack run'
    from inside 'stack test'.

---
+++ runTests.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+set -e
+
+function usage {
+  echo "usage:"
+  echo "  $0 [tests/test-file]"
+  echo
+  echo "If no test file is given, runs all the tests."
+}
+
+function runTest {
+  local TESTFILE="$1"
+  local EXPECT_SCRIPT="$(cat "$TESTFILE" | \
+    sed 's/^\([^>].*\)$/expect {  "\1\\r" {}  timeout { exit 1 }}/g' | \
+    sed 's/^> \(.*\)$/expect {  "> " {}  timeout { exit 1 }}send "\1\\r"/g' | tr '' '\n')"
+
+  #echo "$EXPECT_SCRIPT"
+  #echo
+
+  stack build hyzzy
+  expect -c 'set timeout 3' \
+         -c 'spawn stack run hyzzy' \
+         -f <(echo "$EXPECT_SCRIPT")
+}
+
+if [ "$1" = "--help" -o "$1" = "-h" ]; then
+  usage
+elif [ -f "$1" ]; then
+  runTest "$1"
+  echo "** TEST PASSED **"
+elif [ -z "$1" ]; then
+  for x in tests/*; do
+    runTest "$x"
+  done
+  echo "** ALL TESTS PASS **"
+else
+  (
+    echo "unrecognized arguments: " $@
+    usage
+  ) 1>&2
+  exit 1
+fi
