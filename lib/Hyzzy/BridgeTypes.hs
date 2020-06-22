24a3b97f4bc3ae65b045825a1207dfc932cf26cf
Samuel Gélineau <gelisam+github@gmail.com>
2020-06-21 22:03:28 -0400

    games no longer refer to the inventory directly

--- lib/Hyzzy/BridgeTypes.hs
+++ lib/Hyzzy/BridgeTypes.hs
@@ -1,19 +1,14 @@
 -- The types of all the values transfered between the game and hyzzy.
 module Hyzzy.BridgeTypes
-  ( -- needed for the inventory.
-    Char, Dynamic, Map
-
-    -- needed for the game-specific commands.
-  , Command, CommandF, Coyoneda, Free
+  ( -- needed for the game-specific commands.
+    Command, CommandF, Coyoneda, Free
 
     -- needed for the game-specific objects.
   , Object
   ) where
 
 import Control.Monad.Free
-import Data.Dynamic
 import Data.Functor.Coyoneda
-import Data.Map
 
 import Hyzzy.Command
 import Hyzzy.Object
