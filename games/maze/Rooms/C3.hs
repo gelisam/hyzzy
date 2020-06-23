f574433919e5ea024b23f6d6ff3790e6d7c57a0f
Samuel Gélineau <gelisam+github@gmail.com>
2020-06-23 09:51:35 -0400

    give the player a map
    the maze is too hard...

--- games/maze/Rooms/C3.hs
+++ games/maze/Rooms/C3.hs
@@ -2,11 +2,35 @@
 module Rooms.C3 where
 
 import Hyzzy.Command
+import Hyzzy.Object
+
+import Objects
 
 
 look :: Command
 look = "More foliage walls. This place is a maze..."
 
+map :: Instance Paper
+map = Instance Paper $ PaperFields
+  { paperLines =
+    [ "  1 2 3 4 5 6 7 "
+    , " +-+-+ +-+-+-+-+"
+    , "A|   |         |"
+    , " + +-+-+-+-+-+ +"
+    , "B| |       |   |"
+    , " + + +-+-+ + +-+"
+    , "C| | |     |   |"
+    , " + + +-+ +-+ + +"
+    , "D|   |     | | |"
+    , " + + + + + + + +"
+    , "E| | |   | |   |"
+    , " + + +-+ + +-+ +"
+    , "F| |     |     |"
+    , " + +-+-+ +-+-+ +"
+    , "G|             |"
+    , " +-+-+-+-+-+-+-+"
+    ]
+  }
 
 
 
